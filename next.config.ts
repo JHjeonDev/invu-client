735e45252d3228f9073b17c3c6421f05ff694530
hanseok kim <hasuikhs@gmail.com>
2025-03-09 00:13:20 +0900

    데이터 연결

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from 'next';
 
+const apiBaseUrl = process.env.NODE_ENV === 'development'
+  ? 'https://port-0-invu-api-server-m78pe23vb8056d68.sel4.cloudtype.app'
+  : 'https://port-0-invu-api-server-m78pe23vb8056d68.sel4.cloudtype.app';
+
+const serverApiBaseUrl = process.env.NODE_ENV === 'development'
+  ? 'https://port-0-invu-api-server-m78pe23vb8056d68.sel4.cloudtype.app'
+  : 'invu-api-server:8080';
+
 const nextConfig: NextConfig = {
+  env: {
+    CLIENT_API_BASE_URL: apiBaseUrl,
+    SERVER_API_BASE_URL: serverApiBaseUrl
+  }
   /* config options here */
 };
 
