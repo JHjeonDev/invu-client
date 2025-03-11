19dc0484b485855f6417b3658eebabdeb2b7000b
hanseok kim <hasuikhs@gmail.com>
2025-03-11 23:58:35 +0900

    intersectionObserve margin 옵션 추가

--- src/utils/customHook.ts
+++ src/utils/customHook.ts
@@ -4,7 +4,8 @@ export function useIntersectionObserver(
   ref: React.RefObject<HTMLElement | null>,
   animation: Keyframe[],
   options: KeyframeAnimationOptions,
-  threshold: number = 0.1
+  threshold: number = 0.1,
+  rootMargin: string = '20px'
 ) {
   useEffect(() => {
     if (!ref || !ref.current) {
@@ -16,12 +17,12 @@ export function useIntersectionObserver(
       if (entry.isIntersecting) {
         ref.current?.animate(animation, options);
       }
-    }, { threshold });
+    }, { threshold: threshold, rootMargin: rootMargin });
 
     observer.observe(ref.current);
 
     return () => {
       observer.disconnect();
     };
-  }, [ ref, animation, options, threshold ]);
+  }, [ ref, animation, options, threshold, rootMargin ]);
 }
